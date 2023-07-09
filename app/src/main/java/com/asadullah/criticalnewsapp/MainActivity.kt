3826c2bd3408c42f7e83fa1b30bf78c302a82b98
Asadullah Ilyas <the.elder_wand@yahoo.com>
2023-07-09 16:49:16 +0500

    Colors improved

--- app/src/main/java/com/asadullah/criticalnewsapp/MainActivity.kt
+++ app/src/main/java/com/asadullah/criticalnewsapp/MainActivity.kt
@@ -1,18 +1,15 @@
 package com.asadullah.criticalnewsapp
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.colorResource
 import androidx.fragment.app.FragmentActivity
 import androidx.navigation.compose.rememberNavController
 import com.asadullah.criticalnewsapp.features.NavGraphs
 import com.asadullah.criticalnewsapp.ui.theme.CriticalNewsAppTheme
-import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import com.ramcosta.composedestinations.DestinationsNavHost
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -22,10 +19,6 @@ class MainActivity : FragmentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             CriticalNewsAppTheme {
-                val systemUiController = rememberSystemUiController()
-                systemUiController.setStatusBarColor(
-                    color = colorResource(id = R.color.system_bar_color)
-                )
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
--- app/src/main/java/com/asadullah/criticalnewsapp/ui/theme/Theme.kt
+++ app/src/main/java/com/asadullah/criticalnewsapp/ui/theme/Theme.kt
@@ -13,29 +13,9 @@ import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
+import androidx.compose.ui.res.colorResource
 import androidx.core.view.WindowCompat
-
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
-)
-
-private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
-)
+import com.asadullah.criticalnewsapp.R
 
 @Composable
 fun CriticalNewsAppTheme(
@@ -44,14 +24,36 @@ fun CriticalNewsAppTheme(
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
+    val darkColorScheme = darkColorScheme(
+        primary = colorResource(id = R.color.system_bar_color),
+        secondary = PurpleGrey80,
+        tertiary = Pink80
+    )
+
+    val lightColorScheme = lightColorScheme(
+        primary = colorResource(id = R.color.system_bar_color),
+        secondary = PurpleGrey40,
+        tertiary = Pink40
+
+        /* Other default colors to override
+        background = Color(0xFFFFFBFE),
+        surface = Color(0xFFFFFBFE),
+        onPrimary = Color.White,
+        onSecondary = Color.White,
+        onTertiary = Color.White,
+        onBackground = Color(0xFF1C1B1F),
+        onSurface = Color(0xFF1C1B1F),
+        */
+    )
+
     val colorScheme = when {
         dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
             val context = LocalContext.current
             if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
         }
 
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
+        darkTheme -> darkColorScheme
+        else -> lightColorScheme
     }
     val view = LocalView.current
     if (!view.isInEditMode) {
